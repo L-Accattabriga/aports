a3c64134b834dff74f38638b7915e155f847245e
L-Accattabriga <L-Accattabriga@proton.me>
2025-02-21 19:58:34 +0000

    Update gen-iso.sh

--- gen-iso.sh
+++ gen-iso.sh
@@ -2,17 +2,13 @@
 
 IMAGE=$1
 
-if [ -z "$IMAGE" ]; then
-printf "ERROR\n"
-exit 1
-fi
 xorriso -as mkisofs \
   -o /home/build/iso/$IMAGE.iso \
-  -isohybrid-mbr image-b5c981919b5d9558925571863a2f705c0e6f0bca-x86_64-standard/boot/syslinux/isohdpfx.bin \
-  -c boot/syslinux/boot.cat \
-  -b boot/syslinux/isolinux.bin \
+  -isohybrid-mbr syslinux_b1db609243c8a56d3a840a0750c5d8482011bb60/boot/syslinux/isohdpfx.bin \
+  -c image-b5c981919b5d9558925571863a2f705c0e6f0bca-x86_64-standard/boot/syslinux/boot.cat \
+  -b image-b5c981919b5d9558925571863a2f705c0e6f0bca-x86_64-standard/boot/syslinux/isolinux.bin \
      -no-emul-boot -boot-load-size 4 -boot-info-table \
-  -eltorito-alt-boot -e boot/grub/efi.img \
+  -eltorito-alt-boot -e image-b5c981919b5d9558925571863a2f705c0e6f0bca-x86_64-standard/efi/boot/bootx64.efi \
      -no-emul-boot -isohybrid-gpt-basdat \
   -isohybrid-apm-hfsplus \
   -V "ALPINE_CUSTOM" \
